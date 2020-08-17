5d82d00bb265e7e2a6852225aa1fd67dafeb2454
danieleleto94 <danieleleto94@gmail.com>
2020-08-17 15:47:42 +0200

    Added Client project with relative FileWatcher class

---
+++ Remote_Backup_Client/cmake-build-debug/CMakeFiles/Remote_Backup_Client.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Remote_Backup_Client.dir/FileWatcher.cpp.o"
+  "CMakeFiles/Remote_Backup_Client.dir/main.cpp.o"
+  "Remote_Backup_Client"
+  "Remote_Backup_Client.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Remote_Backup_Client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
